bbd020341fe477a8161895bfd333c42940704cfc
RubenJTL <ruben@ravn.co>
2023-01-15 11:04:12 -0500

    feat: Add initial project

---
+++ sample-dynamic-island/sample_dynamic_islandApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_dynamic_islandApp.swift
+//  sample-dynamic-island
+//
+//  Created by Ruben Torres on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct sample_dynamic_islandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
